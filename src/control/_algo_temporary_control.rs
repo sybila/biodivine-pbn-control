06b9be9f721d45edf7c69fc5ce1521b495e0a865
Eva.Smijakova <eva.smijakova@gmail.com>
2021-03-26 23:15:33 +0100

    Pang models

--- src/control/_algo_temporary_control.rs
+++ src/control/_algo_temporary_control.rs
@@ -4,6 +4,7 @@ use biodivine_lib_param_bn::{BooleanNetwork, VariableId, FnUpdate};
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use crate::control::_algo_tgr_reduction::tgr_reduction;
+use biodivine_lib_param_bn::biodivine_std::bitvector::{ArrayBitVector, BitVector};
 
 
 pub fn reach_bwd_with_saturation(graph: &SymbolicAsyncGraph, initial: GraphColoredVertices) -> GraphColoredVertices {
@@ -67,6 +68,25 @@ fn strong_basin(graph: &SymbolicAsyncGraph, initial: GraphColoredVertices) -> Gr
     }
 }
 
+fn add_auto_regulations(model: BooleanNetwork) -> BooleanNetwork {
+    let mut result = model.as_graph().clone();
+    for v in result.variables() {
+        if result.find_regulation(v, v).is_none() {
+            let name = result.get_variable_name(v).clone();
+            result.add_regulation(name.as_str(), name.as_str(), false, None);
+        }
+    }
+
+    let mut result = BooleanNetwork::new(result);
+    for v in result.variables() {
+        // Technically, the models should have equivalent variable ids!
+        if let Some(function) = model.get_update_function(v) {
+            result.add_update_function(v, function.clone());
+        }
+    }
+
+    result
+}
 
 pub fn find_attractors(graph: &SymbolicAsyncGraph) -> Vec<GraphColoredVertices> {
     let mut result = Vec::new();
@@ -107,17 +127,19 @@ pub fn find_reachable_attractors(graph: &SymbolicAsyncGraph, pivot: GraphColored
 //     }).collect()
 // }
 
-pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap<VariableId, bool>, target: &HashMap<VariableId, bool>) {
+pub fn temporary_source_target_control(network: BooleanNetwork, source: &ArrayBitVector, target: &ArrayBitVector) {
     // First, make a controlled and uncontrolled asynchronous graphs. It is important to make sure
     // the networks have the same regulatory graphs and parameters. This ensures the networks
     // have the same symbolic representation.
 
-    let mut controlled_network = BooleanNetwork::new(network.as_graph().clone());
-    let mut uncontrolled_network = BooleanNetwork::new(network.as_graph().clone());
+    let normalized_network = add_auto_regulations(network);
+
+    let mut controlled_network = BooleanNetwork::new(normalized_network.as_graph().clone());
+    let mut uncontrolled_network = BooleanNetwork::new(normalized_network.as_graph().clone());
 
     // First, copy existing parameters from the original network:
-    for p in network.parameters() {
-        let parameter = network.get_parameter(p);
+    for p in normalized_network.parameters() {
+        let parameter = normalized_network.get_parameter(p);
         let controlled_parameter = controlled_network.add_parameter(parameter.get_name(), parameter.get_arity()).unwrap();
         let uncontrolled_parameter = uncontrolled_network.add_parameter(parameter.get_name(), parameter.get_arity()).unwrap();
         if controlled_parameter != p || uncontrolled_parameter != p {
@@ -126,15 +148,15 @@ pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap
     }
 
     // Then add control parameters and modify update functions:
-    for v in network.variables().rev() {
-        let v_controlled = format!("{}_controlled", network.get_variable_name(v));
+    for v in normalized_network.variables().rev() {
+        let v_controlled = format!("{}_controlled", normalized_network.get_variable_name(v));
         let controlled_parameter = controlled_network.add_parameter(v_controlled.as_str(), 0).unwrap();
         let uncontrolled_parameter = uncontrolled_network.add_parameter(v_controlled.as_str(), 0).unwrap();
         if controlled_parameter != uncontrolled_parameter {
             panic!("This should not happen. Encodings should be the same.");
         }
         let parameter_id = controlled_parameter;
-        if let Some(function) = network.get_update_function(v) {
+        if let Some(function) = normalized_network.get_update_function(v) {
             // A little trick to avoid always cloning the value of fn_parameter...
             let fn_parameter = || FnUpdate::mk_param(parameter_id, &[]);
 
@@ -171,7 +193,7 @@ pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap
     // Now, we can also make some helper sets that represent when variables are controlled:
     let mut var_is_controlled = HashMap::new();
     let mut var_is_not_controlled = HashMap::new();
-    for v in network.variables() {
+    for v in normalized_network.variables() {
         let v_name = controlled_graph.as_network().get_variable_name(v);
         let parameter = controlled_graph.as_network().find_parameter(format!("{}_controlled", v_name).as_str()).unwrap();
         let bdd_v_is_controlled = controlled_graph.symbolic_context().mk_uninterpreted_function_is_true(parameter, &[]);
@@ -192,13 +214,15 @@ pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap
     // First, let's restrict ourselves only to colors where source and target are both members
     // of attractors.
 
-    let target_set = target.iter().fold(uncontrolled_graph.mk_unit_colored_vertices(), |a, (v, value)| {
-        a.intersect(&uncontrolled_graph.fix_network_variable(*v, *value))
-    });
+    let target_set = controlled_graph.vertex(target);
+    // let target_set = target.iter().fold(uncontrolled_graph.mk_unit_colored_vertices(), |a, (v, value)| {
+    //     a.intersect(&uncontrolled_graph.fix_network_variable(*v, *value))
+    // });
 
-    let source_set = source.iter().fold(uncontrolled_graph.mk_unit_colored_vertices(), |a, (v, value)| {
-        a.intersect(&uncontrolled_graph.fix_network_variable(*v, *value))
-    });
+    let source_set = controlled_graph.vertex(source);
+    // let source_set = source.iter().fold(uncontrolled_graph.mk_unit_colored_vertices(), |a, (v, value)| {
+    //     a.intersect(&uncontrolled_graph.fix_network_variable(*v, *value))
+    // });
 
     let target_fwd = reach_fwd_with_saturation(&uncontrolled_graph, target_set.clone());
     let target_bwd = reach_bwd_with_saturation(&uncontrolled_graph, target_set.clone());
@@ -218,7 +242,7 @@ pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap
 
     // To get the actual number of colors, we have to disregard the new parameters (v_controlled)
     // which we have added and are completely unconstrained in this scenario.
-    let state_variable_count = u16::try_from(network.num_vars()).unwrap();  // for each variable, we have added a parameter.
+    let state_variable_count = u16::try_from(normalized_network.num_vars()).unwrap();  // for each variable, we have added a parameter.
     let extra_count = (2.0f64).powi(state_variable_count.into());
     let actual_colors = both_are_attractors.approx_cardinality() / extra_count;
     println!("Both source and target appear in (different) attractors for {} colors.", actual_colors);
@@ -246,8 +270,7 @@ pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap
     // This boils down to removing any state where uncontrolled variables do not match the value
     // in `source` (since control application will fix all controlled variables).
 
-    for v in network.variables() {
-        let source_value = source.get(&v).unwrap().clone();
+    for (v, source_value) in normalized_network.clone().variables().zip(source.values()) {
         let v_different_than_source = uncontrolled_graph.fix_network_variable(v, !source_value);
         let v_not_controlled = var_is_not_controlled.get(&v).unwrap();
 
@@ -267,24 +290,24 @@ pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap
     // to get some basic info from the mapping:
 
     let control_map = controlled_basin;
-
-    for v in network.variables() {
-        let variables_other_then_v_are_not_controlled = var_is_not_controlled.iter()
-            .filter(|(id, _)| **id != v)
-            .fold(uncontrolled_graph.mk_unit_colors(), |a, (_, b)| a.intersect(b));
-        let only_v_is_controlled = var_is_controlled.get(&v).unwrap().intersect(&variables_other_then_v_are_not_controlled);
-
-        // In this symbolic set, all values of `v_controlled` parameters are fixed. We could do
-        // this more easily if we saved `BddVariables` that the `v_controlled` parameters correspond
-        // to, but for now this is ok.
-        let controls_where_only_v_is_controlled = control_map.intersect_colors(&only_v_is_controlled);
-
-        let v_true = uncontrolled_graph.fix_network_variable(v, true);
-        let v_false = uncontrolled_graph.fix_network_variable(v, false);
-
-        // println!("Control [{}=true] works for {} colors.", network.get_variable_name(v), v_true.intersect(&controls_where_only_v_is_controlled).colors().approx_cardinality());
-        // println!("Control [{}=false] works for {} colors.", network.get_variable_name(v), v_false.intersect(&controls_where_only_v_is_controlled).colors().approx_cardinality());
-    }
+    //
+    // for v in normalized_network.variables() {
+    //     let variables_other_then_v_are_not_controlled = var_is_not_controlled.iter()
+    //         .filter(|(id, _)| **id != v)
+    //         .fold(uncontrolled_graph.mk_unit_colors(), |a, (_, b)| a.intersect(b));
+    //     let only_v_is_controlled = var_is_controlled.get(&v).unwrap().intersect(&variables_other_then_v_are_not_controlled);
+    //
+    //     // In this symbolic set, all values of `v_controlled` parameters are fixed. We could do
+    //     // this more easily if we saved `BddVariables` that the `v_controlled` parameters correspond
+    //     // to, but for now this is ok.
+    //     let controls_where_only_v_is_controlled = control_map.intersect_colors(&only_v_is_controlled);
+    //
+    //     let v_true = uncontrolled_graph.fix_network_variable(v, true);
+    //     let v_false = uncontrolled_graph.fix_network_variable(v, false);
+    //
+    //     // println!("Control [{}=true] works for {} colors.", network.get_variable_name(v), v_true.intersect(&controls_where_only_v_is_controlled).colors().approx_cardinality());
+    //     // println!("Control [{}=false] works for {} colors.", network.get_variable_name(v), v_false.intersect(&controls_where_only_v_is_controlled).colors().approx_cardinality());
+    // }
 
     //test
     // let witness = uncontrolled_graph.pick_witness(uncontrolled_graph.unit_colors());
@@ -335,68 +358,68 @@ pub fn temporary_source_target_control(network: BooleanNetwork, source: &HashMap
 }
 
 
-#[cfg(test)]
-mod tests {
-    use biodivine_lib_param_bn::BooleanNetwork;
-    use std::convert::TryFrom;
-    use std::collections::HashMap;
-    use crate::control::_algo_temporary_control::temporary_source_target_control;
+// #[cfg(test)]
+// mod tests {
+//     use biodivine_lib_param_bn::BooleanNetwork;
+//     use std::convert::TryFrom;
+//     use std::collections::HashMap;
+//     use crate::control::_algo_temporary_control::temporary_source_target_control;
+//
+//     #[test]
+//     #[allow(non_snake_case)]
+//     fn test_g2a_control() {
+//         // This is the G2A network which is often used as an example, but here, we have added
+//         // an a self-loop to all variables that otherwise don't have one (because in a controlled
+//         // graph, technically every variable has a self-loop).
+//
+//         // We have also given each variable an explicit update function.
+//         let g2a_network = r"
+//             $CtrA:f_5(CtrA, GcrA, CcrM, SciP)
+//             CtrA -> CtrA
+//             GcrA -> CtrA
+//             CcrM -| CtrA
+//             SciP -| CtrA
+//             $GcrA:f_4(CtrA, DnaA)
+//             GcrA -?? GcrA
+//             CtrA -| GcrA
+//             DnaA -> GcrA
+//             $CcrM:f_3(CtrA, CcrM, SciP)
+//             CtrA -> CcrM
+//             CcrM -| CcrM
+//             SciP -| CcrM
+//             $SciP:f_1(CtrA, DnaA)
+//             CtrA -> SciP
+//             DnaA -| SciP
+//             SciP -?? SciP
+//             $DnaA:f_2(CtrA, GcrA, DnaA, CcrM)
+//             CtrA -> DnaA
+//             GcrA -| DnaA
+//             DnaA -| DnaA
+//             CcrM -> DnaA
+//         ";
+//
+//         let network = BooleanNetwork::try_from(g2a_network).unwrap();
+//         let CcrM = network.as_graph().find_variable("CcrM").unwrap();
+//         let CtrA = network.as_graph().find_variable("CtrA").unwrap();
+//         let DnaA = network.as_graph().find_variable("DnaA").unwrap();
+//         let GcrA = network.as_graph().find_variable("GcrA").unwrap();
+//         let SciP = network.as_graph().find_variable("SciP").unwrap();
+//
+//         let mut source = HashMap::new();
+//         source.insert(CcrM, true);
+//         source.insert(CtrA, true);
+//         source.insert(DnaA, true);
+//         source.insert(GcrA, false);
+//         source.insert(SciP, false);
+//
+//         let mut target = HashMap::new();
+//         target.insert(CcrM, true);
+//         target.insert(CtrA, false);
+//         target.insert(DnaA, false);
+//         target.insert(GcrA, false);
+//         target.insert(SciP, false);
+//
+//         let result = temporary_source_target_control(network, &source, &target);
+//     }
 
-    #[test]
-    #[allow(non_snake_case)]
-    fn test_g2a_control() {
-        // This is the G2A network which is often used as an example, but here, we have added
-        // an a self-loop to all variables that otherwise don't have one (because in a controlled
-        // graph, technically every variable has a self-loop).
-
-        // We have also given each variable an explicit update function.
-        let g2a_network = r"
-            $CtrA:f_5(CtrA, GcrA, CcrM, SciP)
-            CtrA -> CtrA
-            GcrA -> CtrA
-            CcrM -| CtrA
-            SciP -| CtrA
-            $GcrA:f_4(CtrA, DnaA)
-            GcrA -?? GcrA
-            CtrA -| GcrA
-            DnaA -> GcrA
-            $CcrM:f_3(CtrA, CcrM, SciP)
-            CtrA -> CcrM
-            CcrM -| CcrM
-            SciP -| CcrM
-            $SciP:f_1(CtrA, DnaA)
-            CtrA -> SciP
-            DnaA -| SciP
-            SciP -?? SciP
-            $DnaA:f_2(CtrA, GcrA, DnaA, CcrM)
-            CtrA -> DnaA
-            GcrA -| DnaA
-            DnaA -| DnaA
-            CcrM -> DnaA
-        ";
-
-        let network = BooleanNetwork::try_from(g2a_network).unwrap();
-        let CcrM = network.as_graph().find_variable("CcrM").unwrap();
-        let CtrA = network.as_graph().find_variable("CtrA").unwrap();
-        let DnaA = network.as_graph().find_variable("DnaA").unwrap();
-        let GcrA = network.as_graph().find_variable("GcrA").unwrap();
-        let SciP = network.as_graph().find_variable("SciP").unwrap();
-
-        let mut source = HashMap::new();
-        source.insert(CcrM, true);
-        source.insert(CtrA, true);
-        source.insert(DnaA, true);
-        source.insert(GcrA, false);
-        source.insert(SciP, false);
-
-        let mut target = HashMap::new();
-        target.insert(CcrM, true);
-        target.insert(CtrA, false);
-        target.insert(DnaA, false);
-        target.insert(GcrA, false);
-        target.insert(SciP, false);
-
-        let result = temporary_source_target_control(network, &source, &target);
-    }
-
-}
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,47 @@
 use std::fs;
-use biodivine_pbn_control::control::_algo_temporary_control::find_attractors;
+use biodivine_pbn_control::control::_algo_temporary_control::{find_attractors, temporary_source_target_control};
 use biodivine_lib_param_bn::symbolic_async_graph::SymbolicAsyncGraph;
 use biodivine_lib_param_bn::BooleanNetwork;
 use std::convert::TryFrom;
 use biodivine_lib_param_bn::biodivine_std::traits::Set;
+use std::collections::HashMap;
+use std::time::Instant;
 
 fn main() {
-    for m in ["myeloid", "cell_fate", "proinflamatory_tumor", "apoptosis", "cholesterol"].iter() {
-        let witness_str: &str = &fs::read_to_string(format!("models/{}_witness.aeon", m)).unwrap();
+    for m in ["myeloid", "cardiac", "erbb", "tumour", "mapk", "hgf"].iter() {
+    //for m in ["myeloid", "cardiac"].iter() {
+    let witness_str: &str = &fs::read_to_string(format!("models/{}_witness.aeon", m)).unwrap();
         let model = BooleanNetwork::try_from(witness_str).unwrap();
         let graph = &SymbolicAsyncGraph::new(model.clone()).unwrap();
 
 
         println!("Model: {}", m);
-        println!("# variables: {}", model.variables().len());
+        println!("# variables: {}", model.clone().variables().len());
         //println!("{}", graph.unit_colors().approx_cardinality());
 
         let attractors = &find_attractors(graph);
 
         let all = attractors.iter().fold(graph.mk_empty_vertices(), |a, b| a.union(b));
-        println!("# attractors: {}", attractors.len());
+        println!("# attractors: {}", attractors.clone().len());
         println!("# attractor vertices: {}", all.vertices().approx_cardinality());
 
-        for vertex in all.vertices().materialize().iter() {
-            println!("State: {:?}", vertex);
-            let singleton_set = graph.vertex(&vertex);
-            println!("Set vertices: {}", singleton_set.vertices().materialize().iter().count());
+        let mut vertices = Vec::new();
+        for a in attractors {
+            vertices.push(a.pick_vertex());
         }
-        //println!("{:?}", attractors);
-    }
 
+        for (i, s) in vertices.clone().iter().enumerate() {
+            for (j, t) in vertices.clone().iter().enumerate() {
+                for source in s.vertices().materialize().iter() {
+                    for target in t.vertices().materialize().iter() {
+                        let begin = Instant::now();
+                        let _ = temporary_source_target_control(BooleanNetwork::try_from(witness_str).unwrap(), &target, &source);
+                        let elapsed = begin.elapsed().as_millis();
+                        println!("From attractor {} to attractor {} elapsed {} ms", i, j, elapsed);
+                    }
+                }
+
+            }
+        }
+    }
 }
