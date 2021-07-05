1c520edf168ed37f48047cea0a7c9263c76c2660
Eva Šmijáková <smijakova.eva@gmail.com>
2021-07-05 15:01:40 +0200

    Fix suffixes

--- src/main.rs
+++ src/main.rs
@@ -27,36 +27,35 @@ fn main_one_step_old_benchmark() {
     println!(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ONE STEP CONTROL - OLD BENCHMARK");
 
     for m in ["myeloid, cell_fate"].iter() {
-        let suffixes: [&str; 4];
-        let suffixes = if m == "cell_fate" {["7_stable_attractors", "7_stable_attractors_2params", "7_stable_attractors_4params"].to_vec()} else {["witness", "4_params", "8_params", "11params"].to_vec()};
-        
-        for s in suffixes.clone().iter() {
-            let model_string: &str =
-                &std::fs::read_to_string(format!("models/old_benchmark/{}_{}.aeon", m, s)).unwrap();
-            let model = BooleanNetwork::try_from(model_string).unwrap();
-            let perturbations = PerturbationGraph::new(&model);
-            println!("========= {}(v{})(p{}) =========", m, model.num_vars(), perturbations.as_original().unit_colors().approx_cardinality());
-            let start = Instant::now();
-            let attractors = find_witness_attractors(m);
-            println!(
-                "Attractors ready in {}ms, starting control...",
-                start.elapsed().as_millis()
-            );
-            for (t_i, target) in attractors.clone().iter().enumerate() {
-                for (s_i, source) in attractors.clone().iter().enumerate() {
-                    if s_i == t_i {
-                        continue
+        for suffixes in [["witness", "4_params", "8_params", "11params"].to_vec(), ["7_stable_attractors", "7_stable_attractors_2params", "7_stable_attractors_4params"].to_vec()].iter() {
+            for s in suffixes.clone().iter() {
+                let model_string: &str =
+                    &std::fs::read_to_string(format!("models/old_benchmark/{}_{}.aeon", m, s)).unwrap();
+                let model = BooleanNetwork::try_from(model_string).unwrap();
+                let perturbations = PerturbationGraph::new(&model);
+                println!("========= {}(v{})(p{}) =========", m, model.num_vars(), perturbations.as_original().unit_colors().approx_cardinality());
+                let start = Instant::now();
+                let attractors = find_witness_attractors(m);
+                println!(
+                    "Attractors ready in {}ms, starting control...",
+                    start.elapsed().as_millis()
+                );
+                for (t_i, target) in attractors.clone().iter().enumerate() {
+                    for (s_i, source) in attractors.clone().iter().enumerate() {
+                        if s_i == t_i {
+                            continue
+                        }
+                        let start = Instant::now();
+                        let control = perturbations.one_step_control(&source, &target);
+                        println!(
+                            "Control from Attractor {:?} (source) to Attractor {:?} (target) works for {} color(s), jumping through {} vertices.",
+                            s_i,
+                            t_i,
+                            control.controllable_colors_cardinality(),
+                            control.jump_vertices()
+                        );
+                        println!("Elapsed: {}ms", start.elapsed().as_millis());
                     }
-                    let start = Instant::now();
-                    let control = perturbations.one_step_control(&source, &target);
-                    println!(
-                        "Control from Attractor {:?} (source) to Attractor {:?} (target) works for {} color(s), jumping through {} vertices.",
-                        s_i,
-                        t_i,
-                        control.controllable_colors_cardinality(),
-                        control.jump_vertices()
-                    );
-                    println!("Elapsed: {}ms", start.elapsed().as_millis());
                 }
             }
         }
